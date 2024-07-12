d833f9b617d5627e4d522a95caa86ed98a795f67
Rahma-ben-hassen <114585893+Rahma-ben-hassen@users.noreply.github.com>
2024-07-12 12:29:49 +0200

    Project preparation

---
+++ src/main/java/com/example/activate/Controllers/ProjectPreparationController.java
@@ -0,0 +1,43 @@
+package com.example.activate.Controllers;
+
+import com.example.activate.Entities.ProjectPreparation;
+import com.example.activate.Services.ProjectPreparationService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@CrossOrigin
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/project-preparation")
+public class ProjectPreparationController {
+    private final ProjectPreparationService service;
+
+    @PostMapping("/add")
+    public ProjectPreparation addProjectPreparation(@RequestBody ProjectPreparation projectPreparation) {
+        return service.saveEntity(projectPreparation);
+    }
+
+    @PutMapping("/update")
+    public ProjectPreparation updateProjectPreparation(@RequestBody ProjectPreparation projectPreparation) {
+        return service.editEntity(projectPreparation);
+    }
+
+    @GetMapping("/retrieve-all")
+    public List<ProjectPreparation> getAllProjectPreparations() {
+        return service.getAllEntities();
+    }
+
+    @GetMapping("/retrieve/{id}")
+    public ProjectPreparation getProjectPreparation(@PathVariable("id") String id) {
+        return service.getEntity(id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteProjectPreparation(@PathVariable("id") String id) {
+        service.deleteEntity(id);
+    }
+}
